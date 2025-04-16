b1566c3ba76d918a4f5625ebb0bb29bf0b18d659
AliciaWang55 <131703090+AliciaWang55@users.noreply.github.com>
2025-04-16 13:57:33 -0700

    Update ZooKeeperRunner.java

--- ZooKeeperRunner.java
+++ ZooKeeperRunner.java
@@ -91,6 +91,7 @@ public class ZooKeeperRunner
     currentAnimal.speak();
   }
 
+  // 4.9.6
   Animal a = new Elephant("leaves, grasses, roots", false, 60.0);
   a.isNocturnal();
   // a.trumpet();
@@ -99,4 +100,18 @@ public class ZooKeeperRunner
   // a1.sleep();
   // Object a2 = new Animal();
   // a2.sleep();
+
+  Object o = new Object();
+  System.out.println(o.toString());
+  Elephant e = new Elephant("leaves, grasses, roots", false, 60.0);
+  System.out.println(e.toString());
+
+  Animal a3 = new Animal();
+  System.out.println(a3.toString());
+
+  Animal a4 = new Elephant("leaves, grasses, roots", false, 60.0);
+  System.out.println(a4.toString());
+
+  Object o2 = new Elephant("leaves, grasses, roots", false, 60.0);
+  System.out.println(o2.toString());
 }
